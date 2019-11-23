8cba08de26ee3c1ad7f42f1a38aa63a2a219c560
Daniel <garciabusiness21@gmail.com>
2019-11-23 12:22:06 -0600

    second commit

---
+++ ReactWeek1Assignment.js
@@ -0,0 +1,18 @@
+class Student {
+    constructor(name, email, comminity) {
+        this.name = name;
+        this.email = email;
+        this.community = comunity;
+    }
+}
+
+class Bootcamp {
+    constructor(name, level, students = []) {
+        this.name = name;
+        this.level = level;
+        this.students = students;
+    }
+    registerStudent(student){
+        students.filter(n => n );
+    }
+}
